f098460cadce7c9897e8f68a56b1129dcbdda532
okay <okay.zed@gmail.com>
2011-04-07 15:18:01 -0700

    Update simple AI with better logging.

--- ai/simpleai.py
+++ ai/simpleai.py
@@ -1,16 +1,19 @@
 
 import ai
 AIClass="SimpleAI"
+import logging
+log = logging.getLogger(AIClass)
+
 class SimpleAI(ai.AI):
     def _init(self):
-      print self.current_turn
+      log.info("Initializing: Current Turn: %s", self.current_turn)
 
     def _spin(self):
-      print self.my_units
-      print self.visible_enemies
+      log.info("My visible units:%s", self.my_units)
+      log.info("My visible enemies:%s", self.visible_enemies)
 
     def _unit_died(self, unit):
-      print "%s died" % unit
+      log.info("%s died", unit)
 
     def _unit_spawned(self, unit):
-      print "Received a new unit: %s" % unit
+      log.info("Received a new unit: %s", unit)
