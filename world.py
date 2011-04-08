2a0638194875ee74abe4113f22a5bbcaaffa81cc
okay <okay.zed@gmail.com>
2011-04-07 18:46:50 -0700

    Comment out processSpin in world.py

--- world.py
+++ world.py
@@ -214,21 +214,21 @@ class World:
 
         return ai_player
 
-    def processSpin(self, ai):
-      exc_thread = multiprocess.Process(target=ai.turn)
-      try:
-         exc_thread.start()
-         exc_thread.join(AI_CYCLE_SECONDS)
-         if exc_thread.is_alive():
-           log.info("AI %s exceeded execution time of %s seconds",
-                    ai, AI_CYCLE_SECONDS)
-           exc_thread.terminate()
-      except Exception, e:
-          traceback.print_exc()
-          if not settings.IGNORE_EXCEPTIONS:
-            raise
-          log.info("AI raised exception %s, skipping this turn for it", e)
-
+#    def processSpin(self, ai):
+#      exc_thread = multiprocess.Process(target=ai.turn)
+#      try:
+#         exc_thread.start()
+#         exc_thread.join(AI_CYCLE_SECONDS)
+#         if exc_thread.is_alive():
+#           log.info("AI %s exceeded execution time of %s seconds",
+#                    ai, AI_CYCLE_SECONDS)
+#           exc_thread.terminate()
+#      except Exception, e:
+#          traceback.print_exc()
+#          if not settings.IGNORE_EXCEPTIONS:
+#            raise
+#          log.info("AI raised exception %s, skipping this turn for it", e)
+#
     def threadedSpin(self, ai):
       exc_thread = thread2.Thread(target=ai.turn)
       try:
